2ee201b201dddd876c339d8f4be7b628177dc2d9
Fatima-shariq-07 <162745490+FATIMA-SHARIQ294@users.noreply.github.com>
2024-03-14 11:49:14 +0500

    app.js

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ console.log(" 1.\tALIA SHAH\n 2.\tNIMRA KHAN\n 3.\tAYESHA SHARIQ\n 4.\tSIMRA AKM
 var personName = "\n\tBABAR AZAM\n\t";
 console.log(personName);
 var stripped = personName.trim();
-console.log(stripped);
+console.log(stripped); 
